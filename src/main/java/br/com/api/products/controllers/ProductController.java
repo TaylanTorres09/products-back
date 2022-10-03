8a4d4204a81bfcf03de6a2e534a055bd406d453e
TaylanTorres09 <taylantorres@gmail.com>
2022-10-03 20:15:41 -0300

    Change connect MongoDB complete

--- src/main/java/br/com/api/products/controllers/ProductController.java
+++ src/main/java/br/com/api/products/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package br.com.api.products.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +15,17 @@ import br.com.api.products.service.ProductService;
 @RequestMapping("/products")
 public class ProductController {
     
+    @Autowired
     private ProductService productService;
 
-    @GetMapping("")
+    @GetMapping("/")
     public Iterable<Product> listAll() {
         return productService.listAll();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerProduct(@RequestBody Product product) {
+        return productService.registerProduct(product);
     }
 
 }
--- src/main/java/br/com/api/products/service/ProductService.java
+++ src/main/java/br/com/api/products/service/ProductService.java
@@ -1,6 +1,8 @@
 package br.com.api.products.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.products.models.Product;
@@ -15,6 +17,11 @@ public class ProductService {
     // Method list all products
     public Iterable<Product> listAll() {
         return productRepository.findAll();
+    }
+
+    // Method create products
+    public ResponseEntity<?> registerProduct(Product product) {
+        return new ResponseEntity<Product>(productRepository.save(product), HttpStatus.CREATED);
     }
 
 }

18f4eb59bc2da372eb95930dfddfbbf2320c4007
TaylanTorres09 <taylantorres@gmail.com>
2022-10-03 00:37:00 -0300

    Model and Repository of Product

--- src/main/java/br/com/api/products/repository/ProductRepository.java
+++ src/main/java/br/com/api/products/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package br.com.api.products.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.api.products.models.Product;
 
-
+@Repository
 public interface ProductRepository extends MongoRepository<Product, Long> {
     
 }

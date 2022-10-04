57b8a476fd942fe5d651f0b51fb1ce7f91b82a8a
TaylanTorres09 <taylantorres@gmail.com>
2022-10-03 23:59:09 -0300

    Remove unsable lib

--- src/main/java/br/com/api/products/controllers/UsedProductController.java
+++ src/main/java/br/com/api/products/controllers/UsedProductController.java
@@ -1,6 +1,5 @@
 package br.com.api.products.controllers;
 
-import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;

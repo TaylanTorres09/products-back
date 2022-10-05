0f1cfbee52af6a5def8de0d95cb160ebeccbac89
TaylanTorres09 <taylantorres@gmail.com>
2022-10-05 01:28:07 -0300

    Add Validator in User Controller e Model

--- src/main/java/br/com/api/products/controllers/UserController.java
+++ src/main/java/br/com/api/products/controllers/UserController.java
@@ -1,8 +1,13 @@
 package br.com.api.products.controllers;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.products.models.User;
 import br.com.api.products.service.UserService;
+import br.com.api.products.utils.Regex;
 
 
 @RestController
@@ -20,13 +26,20 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private Regex regex;
+
     @GetMapping("/")
     public Iterable<User> listAll() {
         return userService.listAll();
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody User user) {
+    public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult bindingResult) {
+        FieldError fieldError = bindingResult.getFieldError();
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<FieldError>(fieldError, HttpStatus.BAD_REQUEST);
+        }
         return userService.register(user);
     }
 
--- src/main/java/br/com/api/products/models/User.java
+++ src/main/java/br/com/api/products/models/User.java
@@ -1,5 +1,10 @@
 package br.com.api.products.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,8 +18,18 @@ public class User {
     
     @Id
     private String id;
+
+    @NotEmpty(message = "Nome obrigatório")
     private String name;
+
+    @NotBlank(message = "Email obrigatório")
+    @Pattern(
+        regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"
+    )
     private String email;
+
+    @NotBlank(message = "Senha obrigatório")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$")
     private String password;
 
 }

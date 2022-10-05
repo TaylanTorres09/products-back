6b9cf009031da4ae1ad21f75a77c8355842fcaa8
TaylanTorres09 <taylantorres@gmail.com>
2022-10-04 23:32:39 -0300

    Annotation @Service posted

--- src/main/java/br/com/api/products/service/UserService.java
+++ src/main/java/br/com/api/products/service/UserService.java
@@ -3,10 +3,12 @@ package br.com.api.products.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import br.com.api.products.models.User;
 import br.com.api.products.repository.UserRepository;
 
+@Service
 public class UserService {
     
     @Autowired

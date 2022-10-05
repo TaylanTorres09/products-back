e0a594c77a4065b87c465e743c1e0b9655331f7c
TaylanTorres09 <taylantorres@gmail.com>
2022-10-04 23:10:21 -0300

    Create ResponseModel

---
+++ src/main/java/br/com/api/products/models/ResponseModel.java
@@ -0,0 +1,15 @@
+package br.com.api.products.models;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class ResponseModel {
+    
+    private String mensage;
+
+}

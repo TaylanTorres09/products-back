780a3ffbd2a7c0cce445e0faec8750323cbf048c
TaylanTorres09 <taylantorres@gmail.com>
2022-10-07 10:59:27 -0300

    Remove anotation Pattern

--- src/main/java/br/com/api/products/models/User.java
+++ src/main/java/br/com/api/products/models/User.java
@@ -32,13 +32,9 @@ public class User {
     private String name;
 
     @NotBlank(message = "Email obrigatório")
-    @Pattern(
-        regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"
-    )
     private String email;
 
     @NotBlank(message = "Senha obrigatório")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$")
     private String password;
 
     @ManyToMany(fetch = FetchType.LAZY)

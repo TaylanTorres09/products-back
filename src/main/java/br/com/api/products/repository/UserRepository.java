21eaf25c8a2fea39a530d6f42f41dff2a3d2ac56
TaylanTorres09 <taylantorres@gmail.com>
2022-10-07 00:33:33 -0300

    Add new methods in UserRepository

--- src/main/java/br/com/api/products/repository/UserRepository.java
+++ src/main/java/br/com/api/products/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package br.com.api.products.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.api.products.models.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByName(String name);
+
+    Boolean existsByName(String name);
     
+    Boolean existsByEmail(String email);
 }

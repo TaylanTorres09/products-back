75b4f94bb5600ba353cf9588fed6dd321a271565
TaylanTorres09 <taylantorres@gmail.com>
2022-10-07 10:38:14 -0300

    Add route to login user

--- src/main/java/br/com/api/products/controllers/UserController.java
+++ src/main/java/br/com/api/products/controllers/UserController.java
@@ -1,11 +1,20 @@
 package br.com.api.products.controllers;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,6 +25,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.products.models.User;
+import br.com.api.products.payload.request.LoginRequest;
+import br.com.api.products.payload.response.UserInfoResponse;
+import br.com.api.products.security.jwt.JwtUtils;
+import br.com.api.products.security.services.UserDetailsImpl;
 import br.com.api.products.service.UserService;
 import br.com.api.products.utils.Regex;
 
@@ -27,6 +40,12 @@ public class UserController {
     
     @Autowired
     private UserService userService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
 
     @Autowired
     private Regex regex;
@@ -43,6 +62,28 @@ public class UserController {
             return new ResponseEntity<FieldError>(fieldError, HttpStatus.BAD_REQUEST);
         }
         return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager
+            .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+            .map(item -> item.getAuthority())
+            .collect(Collectors.toList());
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+        .body(new UserInfoResponse(userDetails.getId(),
+                                    userDetails.getName(),
+                                    userDetails.getEmail(),
+                                    roles));
     }
 
 }

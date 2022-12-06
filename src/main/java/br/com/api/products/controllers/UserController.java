5aacb3dc2744c0c5021b95746a009ed9d044c023
TaylanTorres09 <taylantorres@gmail.com>
2022-12-06 20:40:34 -0300

    Remove variable not used

--- src/main/java/br/com/api/products/controllers/UserController.java
+++ src/main/java/br/com/api/products/controllers/UserController.java
@@ -7,8 +7,6 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
